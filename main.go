356c8eb8c26b35951c2292df07b45497270aeaf5
Amanda de Oliveira Pinheiro <amandajauregui.dev@gmail.com>
2024-06-10 21:27:04 -0300

    load balancer com go routines

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"time"
+)
 
-func main() {
-	http.HandleFunc("/", HelloWorld)
-	http.ListenAndServe(":3000", nil)
+func Worker(workerId int, data chan int) {
+	for x := range data { //esvazia o canal
+		fmt.Printf("Worker %d recebeut %d\n", workerId, x)
+		time.Sleep(time.Second)
+	}
 }
 
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!")) //write recebe um slice de bytes (array)
+func main() {
+	canal := make(chan int)
+
+	go Worker(1, canal)
+	go Worker(2, canal)
+
+	for i := range 10 {
+		canal <- i //travado aqui até o canal esvaziar. Quando esvaziar o loop continua
+	}
 }
